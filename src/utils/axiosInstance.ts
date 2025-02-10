8046782d2bafbd805362f29bdcd5a12b887adc4a
Arjun Sharma <arjunsharma6622@gmail.com>
2025-02-10 23:35:40 +0530

    added prod BE link

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = process.env.NODE_ENV !== 'production' ? "https://roxiler-be.vercel.app/api" : 'http://localhost:8000/api';
+export const baseURL = process.env.NODE_ENV === 'production' ? "https://roxiler-be.vercel.app/api" : 'http://localhost:8000/api';
 
 const axiosInstance = axios.create({
 	baseURL: baseURL,
